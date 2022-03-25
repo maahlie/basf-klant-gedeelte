ce72f1fa5a70344c7b79efebc7ba102995a06f51
thomas <thomas.maly@student.gildeopleidingen.nl>
2022-03-25 02:21:52 +0100

    mijn request-emp pagina handmatig

--- applicatie_basf/customer/planning/request-employees.php
+++ applicatie_basf/customer/planning/request-employees.php
@@ -6,6 +6,54 @@ session_start();
 
 // Haal de gebruiker op uit de sessie
 $_user = $_SESSION["_user"];
+$db = new DataBase();
+
+if(isset($_POST['_submit_Request']))
+{
+    $_employees = $_POST['_task_Force'];
+    $_date = strtotime($_POST['_task_Date']);
+    $_date = date("Y-m-d", $_date);
+    $_work = $_POST['_work_comp'];
+    $_desc = $_POST['_task_Description'];
+    $_department = $_POST['_crop'];
+
+
+    $_data = array (
+        array("reqStaff", $_employees),
+        array("descr", $_desc),
+        array("date", $_date),
+        array("departmentID", $_department),
+        array("workCompName", $_work),
+        array("userID", $_user->getTableID())
+    );
+
+    // Sla de gegevens op in de database
+    $_user->registerData("task", $_data);
+}
+
+if(isset($_POST['_confirm_change']))
+{
+    $_task_ID = $_POST['_confirm_change'];
+    $_department = $_user->getDepartment();
+    $_employees = $_POST['_force'];
+    $_date = strtotime($_POST['_date']);
+    $_date = date("Y-m-d", $_date);
+    $_crop = $_POST['_crop'];
+    $_work = $_POST['_work'];
+    $_desc = $_POST['_desrciption'];
+
+
+    $_data = array (
+        array("reqStaff", $_employees),
+        array("descr", $_desc),
+        array("date", $_date),
+        array("departmentID", $_crop),
+        array("workCompName", $_work)
+    );
+
+    $db->updateData('task', $_data, 'taskID', $_task_ID);
+
+}
 
 // Controleer op een buttonpress
 if(isset($_POST['_submit_Task']))
@@ -13,9 +61,10 @@ if(isset($_POST['_submit_Task']))
     // Haal waarden uit de inputs
     $_employees = $_POST['_task_Force'];
     $_date = strtotime($_POST['_task_Date']);
-    // Vervoeg de datum voor de database naar een 'yyyy-mm-dd' formaat
     $_date = date("Y-m-d", $_date);
-    $_description = $_POST['_task_Description'];
+    $_crop = $_POST['_task_crop'];
+    $_work = $_POST['_task_work'];
+    $_desc = $_POST['_task_Description'];
 
     // Haal de afdeling op van de gebruiker
     $_department = $_user->getDepartment();
@@ -25,7 +74,9 @@ if(isset($_POST['_submit_Task']))
         array("reqStaff", $_employees),
         array("descr", $_description),
         array("date", $_date),
-        array("departmentID", $_department)
+        array("departmentID", $_department),
+        array("crop", $_crop),
+        array("workCompName", $_work)
     );
 
     // Sla de gegevens op in de database
